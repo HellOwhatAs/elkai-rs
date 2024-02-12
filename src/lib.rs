0741686a854fcfe0cee3ac4492a5f838f08b984d
HellOwhatAs <xjq701229@outlook.com>
2024-02-12 21:44:29 +0800

    commit

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ extern "C" {
     fn _solve_problem(paramsStr: *const libc::c_uchar, problemStr: *const libc::c_uchar, toursize: *mut libc::size_t) -> *mut libc::c_int;
 }
 
-fn elkai_solve_problem(param: &str, problem: &str) -> Vec<i32> {
+pub fn elkai_solve_problem(param: &str, problem: &str) -> Vec<i32> {
 
     assert!(param.ends_with('\0') && problem.ends_with('\0'), "input string must end with '\\0'");
 
@@ -25,7 +25,7 @@ fn elkai_solve_problem(param: &str, problem: &str) -> Vec<i32> {
 
 #[test]
 fn test() {
-    for i in 0..10000 {
+    for _ in 0..10000 {
         let param = "RUNS = 10\nPROBLEM_FILE = :stdin:\n\0".to_string();
         let problem = "TYPE : ATSP\nDIMENSION : 3\nEDGE_WEIGHT_TYPE : EXPLICIT\nEDGE_WEIGHT_FORMAT : FULL_MATRIX\nEDGE_WEIGHT_SECTION\n0 4 0\n0 0 5\n0 0 0\n\0".to_string();
         print!("{:?} ", elkai_solve_problem(&param, &problem));
